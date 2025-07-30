f858eaf5817d808d79e37ec5c029280ee75d67c7
Common Lisper <atosdo@live.com>
2025-07-30 16:25:40 +0300

    add constructor and fields to Player class

--- src/com/github/fourinline/domain/Player.java
+++ src/com/github/fourinline/domain/Player.java
@@ -1,5 +1,20 @@
 package com.github.fourinline.domain;
 
 public class Player {
-    
+    private final String name;
+    private final Token token;
+
+    public Player(String name, Token token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
 }
