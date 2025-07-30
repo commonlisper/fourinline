911c5699059425a76ad509c777971bb75fa431ae
Common Lisper <atosdo@live.com>
2025-07-30 12:53:57 +0300

    add an enum called 'Size' for board sizing.

--- src/com/github/fourinline/domain/Board.java
+++ src/com/github/fourinline/domain/Board.java
@@ -1,5 +1,28 @@
 package com.github.fourinline.domain;
 
 public class Board {
+    
 
+    public enum Size {
+        DEFAULT(6, 7),
+        ROW7_COLUMN8(7, 8),
+        ROW7_COLUMN9(7, 9),
+        ROW7_COLUMN10(7, 10);
+
+        private final int rows;
+        private final int columns;
+
+        Size(int rows, int columns) {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public int getRows() {
+            return rows;
+        }
+
+        public int getColumns() {
+            return columns;
+        }
+    }
 }
